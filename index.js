5e32ab64a04e7242f10d8928aade7beb0dac0d6d
ndrbr <andrey.burdak@gmail.com>
2023-04-04 16:03:23 +0700

    Added GET and POST routing

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const http = require("http");
+import * as http from "http";
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+];
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    // "Content-Type": "text/plain",
-    "Content-Type": "application/json",
-  });
-  //   res.end("Hello from node server!");
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "Sir Isaac Newton",
-    })
-  );
+  const parts = req.url.split("/");
+  if (parts.length > 0 && parts[1] === "friends") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    if (req.method === "GET") {
+      if (parts.length === 2) res.end(JSON.stringify(friends));
+      else {
+        const friendIndex = parts[2];
+        res.end(JSON.stringify(friends[friendIndex]));
+      }
+    } else if (req.method === "POST") {
+      req.on("data", (data) => {
+        const str = data.toString();
+        friends.push(JSON.parse(str));
+      });
+      req.pipe(res);
+    }
+  } else if (req.url === "/message") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<p>Hello page</p>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
